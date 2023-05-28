7737959c479c7522f32f28f55b9bf14da95e4e92
Zoro444 <102549906+Zoro444@users.noreply.github.com>
2023-05-28 15:40:59 +0400

    fix os issue

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import { EventEmitter } from "events";
 
 process.stdin.setEncoding('utf-8');
-process.stdout.write(`Welcome ${process.env.USERNAME}! \n`);
+const userName = process.env.USERNAME ?? process.env.USER;
+process.stdout.write(`Welcome ${userName}!\n`);
 
 const cliEmitter = new EventEmitter();
 
 process.on('SIGINT', () => cliEmitter.emit('.exit'));
 
 cliEmitter.on(".exit", () => {
-    console.log(`Thank you ${process.env.USERNAME}, goodbye!`);
+    console.log(`Thank you ${userName}, goodbye!`);
     process.exit();
 });
 
 cliEmitter.on("os --cpus", () => console.log((process.cpuUsage())));
 cliEmitter.on("os --homedir", () => console.log(process.env.HOME));
-cliEmitter.on("os --username", () => console.log(process.env.USERNAME));
+cliEmitter.on("os --username", () => console.log(userName));
 cliEmitter.on("os --architecture", () => console.log(process.arch));
 cliEmitter.on("os --hostname", () => console.log(process.env.HOSTNAME));
 cliEmitter.on("os --platform", () => console.log(process.platform));
